304eea568a3494a641c1ecdacf5b99bd6943efec
prakash770405 <suryazzz642@gmail.com>
2026-05-25 22:19:56 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const { faker } = require('@faker-js/faker');
+const mysql = require("mysql2");
+
+const app = express();
+
+let port = 8080;
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended:true}));
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'Mysql2',
+    password: 'prakash@123'
+});
+
+
+
+function getrandomuser() {
+
+    return [
+        faker.string.uuid(),
+        faker.internet.username(),
+        faker.internet.email(),
+        faker.image.avatar(),
+        faker.internet.password()
+    ];
+}
+
+
+
+
+app.get("/", (req, res) => {
+    connection.query('SELECT * from randomdata', (error, results) => {
+        if (error) throw error;
+        console.log('The solution is: ', results);
+        res.render("home.ejs",{results});
+    });
+})
+
+
+app.get("/input", (req, res) => {
+
+    let q = 'INSERT INTO randomdata (Id, Name, Email, images, password) values ?';
+     let users = [];
+
+    for (let i = 0; i < 30; i++) {
+        users.push(getrandomuser());
+    }
+    connection.query(q,[users], (error, results) => {
+         if (error) {
+            console.log(error);
+            return res.send("Insert failed");
+        }
+
+        console.log(results);
+
+        res.send("5 random users inserted successfully");
+    });
+
+})
+
+
+
+
+
+
+app.listen(port, (req, res) => {
+    console.log(`app is listening on port http://localhost:${8080}`);
+})
