12c19d2f2915c12e887f28ec81c013cf537c4693
prakash770405 <suryazzz642@gmail.com>
2026-05-26 22:59:52 -0400

    edit route creating

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 let port = 8080;
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -36,21 +36,50 @@ app.get("/", (req, res) => {
     connection.query('SELECT * from randomdata', (error, results) => {
         if (error) throw error;
         console.log('The solution is: ', results);
-        res.render("home.ejs",{results});
+        res.render("home.ejs", { results });
     });
 })
+
+app.get("/post/:Id", (req, res) => {
+    let { Id } = req.params;
+    console.log(Id);
+    connection.query(`SELECT * from randomdata WHERE Id='${Id}'`, (error, results) => {
+        if (error) throw error;
+        console.log('The solution is: ', results);
+        let data = results[0];
+        res.render("editpost.ejs", { data });
+    });
+})
+
+app.patch("/post/:Id", (req, res) => {
+    let { Id } = req.params;
+    let {password}=req.body;
+    console.log(password); 
+    console.log(Id);
+    connection.query(`SELECT * from randomdata WHERE Id='${Id}'`, (error, results) => {
+        if (error) throw error;
+        // connection.query(`UPDATE randomdata SET password='${newpass}' WHERE Id='${Id}'`, (error, results) => {
+        //     if (error) throw error;
+        //     console.log('The solution is: ', results);
+        //     let data = results[0];
+        //     res.render("editpost.ejs", { data });
+        // });
+
+    });
+})
+
 
 
 app.get("/input", (req, res) => {
 
     let q = 'INSERT INTO randomdata (Id, Name, Email, images, password) values ?';
-     let users = [];
+    let users = [];
 
     for (let i = 0; i < 30; i++) {
         users.push(getrandomuser());
     }
-    connection.query(q,[users], (error, results) => {
-         if (error) {
+    connection.query(q, [users], (error, results) => {
+        if (error) {
             console.log(error);
             return res.send("Insert failed");
         }
